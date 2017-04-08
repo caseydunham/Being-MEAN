071d3696281e131837cee698c74b21f9e1ea5915
K. M. Hoodlet <khoodlet@gmail.com>
2017-04-07 20:36:50 -0400

    Add second ssti test case route

---
+++ test-app/routes/ssti_ex2.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+const jade = require('jade');
+
+router.get('/', function(req, res, next) {
+    res.render('ssti_ex2');
+});
+
+router.post('/ssti_ex2', function(req, res, next) {
+    var query = req.body.expr;
+    var template = 'p Hello ' + query;
+    var html = jade.compile(template);
+    res.send(html());
+});
+
+module.exports = router;
