43f1d7c8a2325f3014fb2611cc0d7a22243f5d79
Casey Dunham <casey.dunham@gmail.com>
2017-04-13 19:05:37 -0400

    Update mongodb injection data

--- test-app/models/users.js
+++ test-app/models/users.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
   username: {type: String, required: true}
 });
 
--- test-app/routes/mongodb.js
+++ test-app/routes/mongodb.js
@@ -11,23 +11,59 @@ var User = mongoose.model('User');
 User.remove({}, function(err) {});
 
 // create a few test users
-[['test1'], ['test2'], ['test3'], ['test4']].forEach(function(d) {
+var users = [
+  ['Carol', 'Danvers', 'cdanvers'],
+  ['Susan', 'Storm', 'sstorm'],
+  ['Natasha', 'Romanov', 'nromanov'],
+  ['Jean', 'Grey', 'jgrey']
+];
+
+users.forEach(function(d) {
     var user = new User();
-    user.username = d[0];
+    user.firstName = d[0];
+    user.lastName = d[1];
+    user.username = d[2];
     user.save();
 });
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   var username = req.query.query;
   console.log("query: " + username);
-  User.find({"username": username}, function(err, users) {
+  var query = {
+    $where: "this.username === '" + username + "'"
+  }
+
+  //query = "var data = User.find({username:'jgrey'});return data;";
+  User.find(query, function(err, users) {
     if (err) {
       console.log(err);
+      res.send(err);
+      return;
     }
     console.log('find completed: ' + users);
     res.render('mongodb', {users: users});
   });
 });
 
+
+/* GET home page. */
+/*
+router.get('/', function(req, res, next) {
+  var username = req.query.query;
+  console.log("query: " + username);
+  var query = {
+    $where: "this.username === '" + username + "'"
+  }
+
+  User.find(query, function(err, users) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+      return;
+    }
+    console.log('find completed: ' + users);
+    res.render('mongodb', {users: users});
+  });
+});
+*/
 module.exports = router;
